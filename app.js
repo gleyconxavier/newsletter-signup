1d74857e0e290d9d9b545a204223ab1bc4a6145d
gleyconxavier <gleyconxavier@hotmail.com>
2019-11-05 21:28:41 -0400

    Update | Signup Screen

---
+++ app.js
@@ -0,0 +1,25 @@
+// jshint esversion:6
+const bodyParser = require("body-parser");
+const request = require("request");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+
+  let firstName = req.body.fName;
+  let lastName = req.body.lName;
+  let email = req.body.email;
+  console.log(firstName, lastName, email);
+});
+
+app.listen(3000, function() {
+  console.log("Server is running on 3000");
+});
