119b9ce8c02a88bdd0951dbaff2990fba91340ad
gleyconxavier <gleyconxavier@hotmail.com>
2019-11-06 09:20:52 -0400

    Update | Final

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/post", function(req, res) {
+  res.redirect("/");
 });
 
 app.post("/", function(req, res) {
@@ -19,25 +25,23 @@ app.post("/", function(req, res) {
   let email = req.body.email;
 
   let data = {
-    members: [
-      {
+    members: [{
       email_address: email,
       status: "subscribed",
       merge_fields: {
         FNAME: firstName,
         LNAME: lastName,
       }
-    }
-    ]
+    }]
   };
 
   let jsonData = JSON.stringify(data);
 
   var options = {
-    url: "https://us20.api.mailchimp.com/3.0/lists/",
+    url: "https://us20.api.mailchimp.com/3.0/lists/", // mailchimp list
     method: "POST",
     headers: {
-      "Authorization": ""
+      "Authorization": "" //api key
     },
     body: jsonData
   };
@@ -45,9 +49,13 @@ app.post("/", function(req, res) {
   request(options, function(error, response, body) {
 
     if (error) {
-      console.log(error);
+      res.sendFile(__dirname + "/failure.html");
     } else {
-      console.log(response.statusCode);
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/sucess.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
     }
 
   });
